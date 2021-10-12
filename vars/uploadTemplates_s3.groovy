a112a6ab46bac76394caf8b1f55e8780b4933f81
banaagmb <87474251+banaagmb@users.noreply.github.com>
2021-10-12 19:43:39 +0800

    Update uploadTemplates_s3.groovy

--- vars/uploadTemplates_s3.groovy
+++ vars/uploadTemplates_s3.groovy
@@ -1,4 +1,4 @@
-def call(Map stageParams = [awsRegion: "us-east-1", s3Bucket: "testbucket-anne"]) {
+def call(Map stageParams = [awsRegion: "us-east-1", s3Bucket: "testbucket-miko"]) {
     withAWS(region: stageParams.awsRegion, credentials:"${AWS_CRED}") {
         awsIdentity()
         s3Upload( 
