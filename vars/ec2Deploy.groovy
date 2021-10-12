f66302bc4a7a7b3c1a32982312dc4f1c65f656be
banaagmb <87474251+banaagmb@users.noreply.github.com>
2021-10-12 19:42:00 +0800

    Update ec2Deploy.groovy

--- vars/ec2Deploy.groovy
+++ vars/ec2Deploy.groovy
@@ -2,12 +2,12 @@ def call(Map stageParams = [awsRegion: "us-east-1"]){
   withAWS(region: stageParams.awsRegion, credentials:"${AWS_CRED}") {
         awsIdentity()
         cfnCreateChangeSet(
-          stack:'EC2Jenkins-Anne', 
+          stack:'EC2Jenkins-Miko', 
           changeSet:'my-change-set', 
-          url:'https://testbucket-anne.s3.amazonaws.com/deploy.yml'
+          url:'https://testbucket-miko.s3.amazonaws.com/deploy.yml'
         )
         cfnExecuteChangeSet(
-          stack:'EC2Jenkins-Anne', 
+          stack:'EC2Jenkins-Miko', 
           changeSet:'my-change-set'
         )
   }
